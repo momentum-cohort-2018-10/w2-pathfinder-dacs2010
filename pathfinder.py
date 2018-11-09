c260344047b8c0089fc4956bd94f63fc909c5ac1
xXDuckXx <dacs2010@gmail.com>
2018-11-09 13:34:35 -0500

    made Map class, works on any file, still washed out

--- pathfinder.py
+++ pathfinder.py
@@ -1,58 +1,70 @@
 from PIL import Image
 
+class Map:
+    def __init__(self, name):
+        self.name = name
 
+    def make_color_code_map(self, file):
+    # take hight map and build color map
+        with open(file) as file:
+            # for thing in file:
+                # this nested list comprehention 
+                # makes a float and removes white space... 
+                # for each item in the list... 
+                # in the array
+            elevation_map = [[float(
+                smaller_thing) for smaller_thing in thing.split()] for thing in file.readlines()] 
 
-# take hight map and build color map
-with open("elevation_small.txt") as file:
-    for word in file:
-        # this nested list comprehention 
-        # makes a float and removes white space... 
-        # for each item in the list... 
-        # in the array
-        elevation_map = [[float(
-            smaller_thing) for smaller_thing in thing.split()] for thing in file.readlines()] 
+            # find the highest elevation
+            
+            max_list = [max(row) for row in elevation_map]
+            max_elevation = max(max_list)
+        
+            # for each elevation, 
+            # make each an int (this removes decimal)
+            # divide by the maximum elevation...
+            # then multiply by 255... 
+            # for each item in the list...
+            # in the array
+            color_code_map = [[int(
+                ((each/max_elevation)*255)) for each in num_list] for num_list in elevation_map]
+            # print(color_code_map)
 
-        # find the highest elevation
-        # take the max of the max of the elevation_map
-        max_elevation = max(max(elevation_map))
-       
-        # for each elevation, 
-        # make each an int (this removes decimal)
-        # divide by the maximum elevation...
-        # then multiply by 255... 
-        # for each item in the list...
-        # in the array
-        color_code_map = [[int(
-            ((each/max_elevation)*255)) for each in num_list] for num_list in elevation_map]
-        # print(color_code_map)
+            # testing  initial concept 
+            # color_code_map = [[int(
+            #     (each/40)) for each in num_list] for num_list in elevation_map]
+        return color_code_map
 
-        # testing  initial concept 
-        # color_code_map = [[int(
-        #     (each/40)) for each in num_list] for num_list in elevation_map]
+    # print(make_color_code_map('elevation_small.txt'))
 
+    # # get height and width
+    # # height is the number of lists
+    # height = len(elevation_map[0])
+    # # print(height)
+    # # width is the length of each list (number of rows)
+    # width = len(elevation_map)
+    # # print(width)
 
-# get height and width
-# height is the number of lists
-height = len(elevation_map[0])
-# print(height)
-# width is the length of each list (number of rows)
-width = len(elevation_map)
-# print(width)
+    # y value is the first index of every row
+    # x value is the next index sequentially
 
-# y value is the first index of every row
-# x value is the next index sequentially
-
-def make_map(color_code_map):
-    '''
-    takes a color_code_map and outputs a png file
-    '''
-    test = Image.new('RGB', (height, width), (238, 255, 200))
-    for y, row in enumerate(color_code_map):
-        for x, num in enumerate(row):
-            test.putpixel((x,y), (num, num, num))
-    return test.save("test9.png")       
+    def make_height_map_png(self, file):
+        '''
+        takes a color_code_map and outputs a png file
+        '''
+        color_code_map = self.make_color_code_map(file)
+        # height is the number of lists
+        height = len(color_code_map[0])
+        # width is the length of each list (number of rows)
+        width = len(color_code_map)
+        test = Image.new('RGB', (height, width))
+        for y, row in enumerate(color_code_map):
+            for x, num in enumerate(row):
+                test.putpixel((x,y), (num, num, num))
+        return test.save(self.name)       
     
-make_map(color_code_map)
+map = Map("test15.png")
+map.make_height_map_png('elevation_small.txt')
 
 # # make an image
 # # set height
